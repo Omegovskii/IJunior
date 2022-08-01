e949a4b92a7893a78d64cabf47969a00057fa5bb
Omegovskii <ilchenkoleg01@gmail.com>
2022-08-01 19:26:16 +1000

    Ex.6

---
+++ Ex.6/Scripts/EnemySpawn.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] private Transform _spawn;
+    [SerializeField] private GameObject _enemy;
+
+    private Transform[] _spawnPoints;
+    private WaitForSeconds _spawnTime;
+
+    private void Start()
+    {
+        _spawnTime = new WaitForSeconds(2f);
+        _spawnPoints = new Transform[_spawn.childCount];
+
+        for (int i = 0; i < _spawn.childCount; i++)
+        {
+            _spawnPoints[i] = _spawn.GetChild(i);
+        }
+
+        StartCoroutine(CreateEnemies());
+    }
+
+    private IEnumerator CreateEnemies()
+    {
+        while (true)
+        {
+            Transform targetSpawn = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+            GameObject enemy = Instantiate(_enemy, targetSpawn.position, Quaternion.identity);
+
+            yield return _spawnTime;
+        }
+    }
+}
